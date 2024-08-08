543e4b6a8da1ac3821f05ac38895c9dc4a3763ad
Néstor Vázquez <ng.vazquez02@gmail.com>
2024-08-08 15:48:40 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,25 @@ import Head from 'next/head';
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from './components/AboutSection';
+import { Metadata } from 'next';
+
+const globalMetadata: Metadata = {
+  title: 'Mi Sitio Web',
+  description: 'Descripción de mi sitio web',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Mi Sitio Web',
+    description: 'Descripción de mi sitio web',
+    url: 'https://mi-sitio.com',
+    images: ['https://mi-sitio.com/imagen.jpg']
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Mi Sitio Web',
+    description: 'Descripción de mi sitio web',
+    images: 'https://mi-sitio.com/imagen.jpg'
+  }
+};
 
 export default function Home() {
   return (
