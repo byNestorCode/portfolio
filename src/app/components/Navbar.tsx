2d4f1a6002e70981773bb3200adea08bc55e9e98
Néstor Vázquez <ng.vazquez02@gmail.com>
2024-11-05 13:49:51 -0600

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -24,8 +24,8 @@ const navLinks = [
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
-        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-            <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-85">
+            <div className="flex flex-wrap items-center justify-around mx-auto px-4 py-2">
                 <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
                     🚀
                 </Link>
