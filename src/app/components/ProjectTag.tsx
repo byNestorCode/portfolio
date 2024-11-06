1a6a8202ef860b260c70913e499261c2f6a6c7b9
Néstor Vázquez <ng.vazquez02@gmail.com>
2024-11-05 20:40:47 -0600

    update button color

--- src/app/components/ProjectTag.tsx
+++ src/app/components/ProjectTag.tsx
@@ -7,7 +7,7 @@ interface TagProps {
 }
 
 const ProjectTag: React.FC<TagProps> = ({ name, onClick, isSelected }) => {
-    const buttonStyles = isSelected ? "text-white bg-purple-500 border-purple-500" : "text-[#ADB7BE] border-slate-600 hover:border-white";
+    const buttonStyles = isSelected ? "text-white bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500" : "text-[#ADB7BE] border-slate-600 hover:border-white";
     return (
         <button
             className={`rounded-full border-2 px-6 py-3 text-xl cursor-pointer ${buttonStyles}`}
