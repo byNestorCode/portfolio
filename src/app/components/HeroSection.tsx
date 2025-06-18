d1806fccb62549810e2bf1a29d13208ba44c6279
Néstor Vázquez <ng.vazquez02@gmail.com>
2025-06-18 08:50:12 -0600

    Update HeroSection.tsx

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Image from 'next/image';
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-import Orb from "./orb";
+import Orb from "./Orb";
 
 const HeroSection = () => {
     return (
