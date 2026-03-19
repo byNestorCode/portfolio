83cdf9eb3ef91fb1498110c9a4054ee2ae908d6d
byNestorCode <ng.vazquez02@gmail.com>
2026-03-19 13:00:01 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 import ProjectSection from './components/ProjectSection';
 import GlassmorphNavbar from "./components/GlassmorphNavbar";
 import Providers from "./components/Providers";
+import Grainient from "./components/Grainient";
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://nestorvazquez.mx/'),
@@ -30,9 +31,35 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <Providers>
-      <main className="flex min-h-screen flex-col bg-[url(/images/fondo.svg)] bg-cover">
+      <main className="relative flex min-h-screen flex-col bg-transparent">
+        <div className="fixed inset-0 z-0 pointer-events-none">
+          <Grainient
+            color1="#006d8f"
+            color2="#002e7a"
+            color3="#B19EEF"
+            timeSpeed={0.25}
+            colorBalance={0}
+            warpStrength={1}
+            warpFrequency={5}
+            warpSpeed={2}
+            warpAmplitude={50}
+            blendAngle={0}
+            blendSoftness={0.05}
+            rotationAmount={500}
+            noiseScale={2}
+            grainAmount={0.1}
+            grainScale={2}
+            grainAnimated={false}
+            contrast={1.5}
+            gamma={1}
+            saturation={1}
+            centerX={0}
+            centerY={0}
+            zoom={0.9}
+          />
+        </div>
         <GlassmorphNavbar />
-        <div className="container mx-auto px-12">
+        <div className="container mx-auto px-12 relative z-10">
           <HeroSection />
           <AboutSection />
           <ProjectSection />
